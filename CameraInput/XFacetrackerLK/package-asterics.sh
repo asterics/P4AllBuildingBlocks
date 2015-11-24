b6f9365b3bf8b56fc5d405cc0dc3895483d504bf
deinhofer <deinhofe@technikum-wien.at>
2015-11-24 11:40:13 +0100

    build script now supports building a debian package targets: do-deploy-deb: target with embedded java do-deploy-no-runtime: target without embedded java
    still a lot of hardcoded stuff, has to be improved

--- CameraInput/XFacetrackerLK/package-asterics.sh
+++ CameraInput/XFacetrackerLK/package-asterics.sh
@@ -1,5 +1,5 @@
 echo deploying jars, creating installer
-#javafxpackager -createjar -v -srcdir build/merged/bin/ARE -destdir build/merged/bin/ARE -outfile org.eclipse.osgi_3.6.0.v20100517 -appclass org.eclipse.core.runtime.adaptor.EclipseStarter -paramfile build/merged/bin/ARE/profile/appproperties.ini
+#javapackager -createjar -v -srcdir build/merged/bin/ARE -outdir build/merged/bin/ARE -outfile org.eclipse.osgi_3.6.0.v20100517.jar -appclass org.eclipse.core.runtime.adaptor.EclipseStarter -paramfile build/merged/bin/ARE/profile/appproperties.ini
 
-javafxpackager -deploy -allpermissions -srcdir build/merged/bin/ARE -outdir build/deploy -outfile XFacetrackerLK -width 400 -height 400 -appclass org.eclipse.core.runtime.adaptor.EclipseStarter -paramfile build/merged/bin/ARE/profile/appproperties.ini -name XFacetrackerLK -version "1.0.0-AsTeRICS-2.7" -title "Head-controlled camera mouse" -vendor "UAS Technikum Wien - AsTeRICS" -v -native installer
+javapackager -deploy -srcdir build/merged/bin/ARE -outdir build/deploy -outfile XFacetrackerLK -appclass org.eclipse.core.runtime.adaptor.EclipseStarter -paramfile build/merged/bin/ARE/profile/appproperties.ini -name XFacetrackerLK -title "Head-controlled camera mouse" -vendor "UAS Technikum Wien - AsTeRICS" -v -native deb
 
